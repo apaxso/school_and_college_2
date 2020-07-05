f2e1b849ba140c7d4b53029baff2bcfecf2908f3
Sayeth_We <powerplanets@aol.com>
2020-07-05 16:28:00 -0400

    File Uploading - Just Images for Now With Node.js and MongoDB

---
+++ root/app.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const app = express();
+
+const mongoURI = "mongodb://localhost:27017/file-upload";
+
+app.use(express.json());
+app.set("view engine","ejs");
+
+app.get("/",(req,res) => {
+  if(!gfs) {
+    console.log("some error occured, check connection to db");
+    res.send("some error occured, check connection to db");
+    process.exit(0);
+  }
+  gfs.find().toArray((err, files) => {
+    // check if files
+    if (!files || files.length === 0) {
+      return res.render("index", {
+        files: false
+      });
+    } else {
+      const f = files
+      .map(file => {
+        if (
+          file.contentType === "image/png" ||
+          file.contentType === "image/jpeg"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+          return file;
+      })
+      .sort((a, b) => {
+        return (
+          new Date(b["uploadDate"]).getTime() -
+          new Date(a["uploadDate"]).getTime()
+        );
+      });
+      return res.render("index", {
+        files:f
+      });
+    }
+  });
+});
+
+const conn = mongoose.createConnection(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let gfs;
+conn.once("open", ()=>{
+    gfs=new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: "uploads"
+        });
+});
+
+app.use(express.json());
+app.set("view engine","ejs");
+
+const port = 5001;
+
+app.listen(port, ()=> {
+    console.log("server started on port " + port);
+})
+
+const storage = new GridFsStorage({
+    url:mongoURI,
+    file: (req,file)=>{
+        return new Promise((resolve,reject) => {
+            crypto.randomBytes(16, (err,buf) => {
+                if(err) {
+                    return reject(err);
+                }
+                const filename = buf.toString("hex") + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: "uploads"
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+
+const upload = multer({
+    storage
+});
+
+app.post("/upload",upload.single("file"),(req,res) => {
+    res.redirect("/");
+});
+
+app.get("/image/:filename", (req, res) => {
+  // console.log('id', req.params.id)
+  const file = gfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist"
+        });
+      }
+      gfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+app.post("/files/del/:id", (req, res) => {
+  gfs.delete(new mongoose.Types.ObjectId(req.params.id), (err, data) => {
+    if (err) return res.status(404).json({ err: err.message });
+    res.redirect("/");
+  });
+});
